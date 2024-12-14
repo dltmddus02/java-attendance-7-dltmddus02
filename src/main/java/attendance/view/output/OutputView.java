1f6a8f6755441b3eb7f1a9ac786f9719bce6872a
dltmddus <tmddus4671@gmail.com>
2024-12-14 15:44:16 +0900

    docs(README.md) : 수정할 날짜와 시간 입력 구현 완료 체크

--- src/main/java/attendance/view/output/OutputView.java
+++ src/main/java/attendance/view/output/OutputView.java
@@ -1,6 +1,9 @@
 package attendance.view.output;
 
+import static attendance.view.output.OutputMessage.ATTENDANCE;
 import static attendance.view.output.OutputMessage.INPUT_ATTENDANCE_TIME;
+import static attendance.view.output.OutputMessage.INPUT_ATTENDANCE_TIME_TO_MODIFY;
+import static attendance.view.output.OutputMessage.INPUT_DATE_TO_MODIFY;
 import static attendance.view.output.OutputMessage.INPUT_FEATURE;
 import static attendance.view.output.OutputMessage.INPUT_NICKNAME;
 
@@ -15,5 +18,17 @@ public class OutputView {
 
     public static void printAttendanceTime() {
         System.out.println(INPUT_ATTENDANCE_TIME.getMessage());
+    }
+
+    public static void printAttendance(int todayMonth, int todayDay, String todayOfTheWeek, String attendanceTime) {
+        System.out.printf(ATTENDANCE.getMessage(), todayMonth, todayDay, todayOfTheWeek, attendanceTime, "출석");
+    }
+
+    public static void printDateToModify() {
+        System.out.println(INPUT_DATE_TO_MODIFY.getMessage());
+    }
+
+    public static void printAttendanceTimeToModify() {
+        System.out.println(INPUT_ATTENDANCE_TIME_TO_MODIFY.getMessage());
     }
 }
